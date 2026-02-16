d4209970f240ab0008271923067acaea2d7e5812
lunhect <hlunamoreno17@gmail.com>
2026-02-16 12:01:00 +0100

    Repository

---
+++ src/main/java/es/fplumara/dam1/prestamos/model/Material.java
@@ -0,0 +1,44 @@
+package es.fplumara.dam1.prestamos.model;
+
+import java.util.Set;
+
+public abstract class Material implements Identificable  {
+      private String id;
+      private String nombre;
+      EstadoMaterial estado;
+      Set<String> etiquetas;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public EstadoMaterial getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoMaterial estado) {
+        this.estado = estado;
+    }
+
+    public Set<String> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(Set<String> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+    abstract String getTipo();
+
+}
