89277f35ee163dcb581cbb7a980e7af600b842b2
lunhect <hlunamoreno17@gmail.com>
2026-02-18 12:42:48 +0100

    ...

--- src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
+++ src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
@@ -1,11 +1,36 @@
 package es.fplumara.dam1.prestamos.service;
 
+import es.fplumara.dam1.prestamos.model.Material;
+import es.fplumara.dam1.prestamos.model.Prestamo;
+import es.fplumara.dam1.prestamos.repository.MaterialRepositoryImpl;
+import es.fplumara.dam1.prestamos.repository.Repository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 class PrestamosServiceTest {
 
     // TODO (alumnos): añadir JUnit 5 y Mockito en el pom.xml y completar:
     //
     // - crearPrestamo_ok_cambiaEstado_y_guarda()
+
+    @Mock
+    Repository<Material> materialRepository;
+    Repository<Prestamo> prestamoRepository
+    void crearPrestamo_Ok_cambiaEstadoyGuarda(){
+
+
+
+
+        Optional<Material> materialOpt = materialRepository.findById(idMaterial);
+
+
+    }
+
+
     // - crearPrestamo_materialNoExiste_lanzaNoEncontrado()
     // - crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible()
     // - devolverMaterial_ok_cambiaADisponible()
