7c7fce8613c17420ccdc355ac03b24a72c2500a5
lunhect <hlunamoreno17@gmail.com>
2026-02-19 10:41:56 +0100

    tests, ult

--- src/main/java/es/fplumara/dam1/prestamos/model/Portatil.java
+++ src/main/java/es/fplumara/dam1/prestamos/model/Portatil.java
@@ -1,14 +1,21 @@
 package es.fplumara.dam1.prestamos.model;
 
+import java.util.Set;
+
 public class Portatil extends Material {
 private int ramGB;
+
+
+
 
     public int getRamGB() {
         return ramGB;
     }
 
-    public Portatil(int ramGB) {
-        super();
+
+
+    public Portatil(String id, String nombre, EstadoMaterial estado, Set<String> etiquetas, int ramGB) {
+        super(id, nombre, estado, etiquetas);
         this.ramGB = ramGB;
     }
 
--- src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
+++ src/test/java/es/fplumara/dam1/prestamos/service/PrestamoServiceTest.java
@@ -1,9 +1,8 @@
 package es.fplumara.dam1.prestamos.service;
 
-import es.fplumara.dam1.prestamos.model.EstadoMaterial;
-import es.fplumara.dam1.prestamos.model.Material;
-import es.fplumara.dam1.prestamos.model.Prestamo;
-import es.fplumara.dam1.prestamos.model.Proyector;
+import es.fplumara.dam1.prestamos.exception.MaterialNoDisponibleException;
+import es.fplumara.dam1.prestamos.exception.NoEncontradoException;
+import es.fplumara.dam1.prestamos.model.*;
 import es.fplumara.dam1.prestamos.repository.MaterialRepositoryImpl;
 import es.fplumara.dam1.prestamos.repository.Repository;
 import org.junit.jupiter.api.Assertions;
@@ -12,12 +11,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static java.time.LocalTime.now;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -51,7 +53,7 @@ class PrestamosServiceTest {
     void crearPrestamo_Ok_cambiaEstadoyGuarda() {
 
         String idMaterial = "6769HQ";
-        Proyector proyectorPrestado = new Proyector();
+        Proyector proyectorPrestado = new Proyector("0000", "HP", EstadoMaterial.BAJA, new HashSet<>(), 9);
         proyectorPrestado.setId(idMaterial);
         proyectorPrestado.setEstado(EstadoMaterial.DISPONIBLE);
         proyectorPrestado.setEtiquetas(new HashSet<>());
@@ -62,13 +64,13 @@ class PrestamosServiceTest {
 
         //act, creo el prestamo
 
-        prestamoService.crearPrestamo(idMaterial, "Ivan", LocalDate.now());
+        prestamoService.crearPrestamo(idMaterial, "IVANGOAT", LocalDate.now());
 
 
         //assert
-        verify(prestamoRepository).save(any(Prestamo.class)); //no lo entiendo, 19!
+        verify(prestamoRepository).save(any()); //no lo entiendo, 19!
 
-          //estado del proyector
+        //estado del proyector
 
         assertEquals(EstadoMaterial.PRESTADO, proyectorPrestado.getEstado());
 
@@ -81,12 +83,43 @@ class PrestamosServiceTest {
     // - crearPrestamo_materialNoExiste_lanzaNoEncontrado()
 
     @Test
-    void crearPrestamo_materialNoexiste_lanzaNoencontrado(){
-        String idmaterial
+    void crearPrestamo_materialNoexiste_lanzaNoencontrado() {
+
+
+        NoEncontradoException ex = assertThrows(NoEncontradoException.class, () -> prestamoService.crearPrestamo("604895J", "JOSELUGOAT", LocalDate.now())
+        );
 
     }
+
     // - crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible()
+    @Test
+    void crearPrestamo_materialNoDisponible_lanzaMaterialNoDisponible(){
+
+        when(materialRepository.findById("00067")).thenReturn(Optional.of(new Proyector("00067", "HP", EstadoMaterial.BAJA, new HashSet<>(), 6)));
+
+
+        MaterialNoDisponibleException ex = assertThrows(MaterialNoDisponibleException.class, () -> prestamoService.crearPrestamo("00067", "AINARAPRO", LocalDate.now()));
+
+
+
+    }
     // - devolverMaterial_ok_cambiaADisponible()
+
+
+    void devolverMaterial_ok_cambiaAdisponible(){
+
+
+
+
+
+
+    }
     //
     // Requisito: usar mocks de repositorios y verify(...)
 }
+
+
+
+
+
+
